4ac63bfa024ce249d813fa2c65075b58565a96b9
Lucas <lucasclaudinocc@gmail.com>
2023-11-12 14:04:38 -0300

    Added paddles functionality, including event listeners to key presses

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,9 +3,18 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 public class GameFrame extends JFrame{
-    // The window frame that has minimize, maximize, etc.
-    GamePanel panel = new GamePanel();
+    // The window frame that has "minimize/maximize buttons", etc.
+    GamePanel panel;
     GameFrame() {
         // How the game is represented
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.util.*;
 public class GamePanel extends JPanel implements Runnable{
     static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.555));
     static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
     static final int BALL_DIAMETER = 20;
     static final int PADDLE_WIDTH = 25;
@@ -19,18 +19,35 @@ public class GamePanel extends JPanel implements Runnable{
     Score score;
     GamePanel() {
         // How the game is represented
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
     }
     public void newBall() {
         // How a new ball is created
     }
     public void newPaddles() {
         // How a new paddle is created
+        paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH, PADDLE_HEIGHT, 1);
+        paddle2 = new Paddle(GAME_WIDTH - PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH, PADDLE_HEIGHT, 2);
     }
     public void paintComponent(Graphics g) {
         // How the game is painted
+        image = createImage(getWidth(),getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image,0,0,this);
     }
     public void draw(Graphics g) {
         // How the game is drawn
+        paddle1.draw(g);
+        paddle2.draw(g);
     }
     public void move() {
         // How the game is moved
@@ -40,14 +57,34 @@ public class GamePanel extends JPanel implements Runnable{
     }
     public void run() {
         // How the game is run
+        // LOOP
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now;
+            if(delta >= 1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
     }
     public class AL extends KeyAdapter {
         // How the game handles key presses
         public void keyPressed(KeyEvent e) {
             // How the game handles key presses
+            paddle1.keyPressed(e);
+            paddle2.keyPressed(e);
         }
         public void keyReleased(KeyEvent e) {
             // How the game handles key releases
+            paddle1.keyReleased(e);
+            paddle2.keyReleased(e);
         }
     }
 }
